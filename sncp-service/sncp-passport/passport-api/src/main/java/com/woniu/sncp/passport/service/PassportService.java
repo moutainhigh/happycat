70f10d7ec0288e2fb32fc40a65674cd6ccc59b1d
chenyx <chenyx@snail.com>
2016-05-09 10:51:56 +0800

    添加根据帐号ID查询的接口

--- sncp-service/sncp-passport/passport-api/src/main/java/com/woniu/sncp/passport/service/PassportService.java
+++ sncp-service/sncp-passport/passport-api/src/main/java/com/woniu/sncp/passport/service/PassportService.java
@@ -8,6 +8,7 @@ import com.woniu.sncp.passport.exception.PassportNotFoundException;
 
 /**
  * 帐号相关服务接口
+ * 
  * @author chenyx
  * @date 2016年5月4日
  */
@@ -18,12 +19,28 @@ public interface PassportService {
 	 * 
 	 * @param passportOrAliase
 	 * @return 帐号信息{@link com.woniu.sncp.passport.dto.PassportDto}
+	 * @throws PassportNotFoundException
+	 *             帐号未找到
+	 * @throws PassportHasFrozenException
+	 *             帐号被冻结
+	 * @throws PassportHasLockedException
+	 *             帐号被锁定
+	 * @throws SystemException
+	 *             系统异常
+	 */
+	public PassportDto findPassportByAccountOrAliase(String passportOrAliase)
+			throws PassportNotFoundException, PassportHasFrozenException, PassportHasLockedException, SystemException;
+
+	/**
+	 * 根据帐号ID查询帐号信息
+	 * @param aid 帐号ID
+	 * @return 帐号信息{@link com.woniu.sncp.passport.dto.PassportDto}
 	 * @throws PassportNotFoundException 帐号未找到
 	 * @throws PassportHasFrozenException 帐号被冻结
 	 * @throws PassportHasLockedException 帐号被锁定
-	 * @throws SystemException 系统异常
+	 * @throws SystemException  系统异常
 	 */
-	public PassportDto findPassportByAccountOrAliase(String passportOrAliase)
+	public PassportDto findPassportByAid(Long aid)
 			throws PassportNotFoundException, PassportHasFrozenException, PassportHasLockedException, SystemException;
 
 }
