b253c73f60fa7d095a5068fd21c7f60f90d38d2c
luzz <luzz@snail.com>
2016-05-09 11:04:33 +0800

    添加FcmGameProfile repository

---
+++ sncp-service/sncp-fcm/fcm-mongo/src/main/java/com/woniu/sncp/fcm/mongo/FcmGameProfilePo.java
@@ -0,0 +1,37 @@
+package com.woniu.sncp.fcm.mongo;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * mongoDB对象
+ * @author chenyx
+ * @date 2016年5月4日
+ */
+public class FcmGameProfilePo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	
+	private String gameId;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(String gameId) {
+		this.gameId = gameId;
+	}
+
+}
