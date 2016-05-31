b1d69202e9e0de465971cb278c16c81d0c4f3e00
iekr <iekr@qq.com>
2016-05-31 14:40:14 +0800

    修改cmd命令

--- sncp-cbss/cbss.business/version.1.0.0/api.fcm/trunk/sncp-fcm.bat
+++ sncp-cbss/cbss.business/version.1.0.0/api.fcm/trunk/sncp-fcm.bat
@@ -1,5 +1,4 @@
-e:
-cd E:\workspace_new\sncp-web\api.cbss.woniu.com\sncp-bss\trunk\cbss.business\version.1.0.0\api.fcm\trunk
-ECHO %CD%
+%drv%
+cd %CD%
 mvn clean install -Dmaven.test.skip=true
-pasue
+explorer %CD%
--- sncp-cbss/cbss.core/cbss.parent/prod_war.bat
+++
@@ -1,4 +0,0 @@
-%drv%
-cd %CD%
-mvn clean install -Dmaven.test.skip=true
-explorer %CD%
