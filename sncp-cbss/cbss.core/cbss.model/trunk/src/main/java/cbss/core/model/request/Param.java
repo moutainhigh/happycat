8f9798fdae69e8fda2326afd510d916fd13365ed
maocc@snail.com <maocc@snail.com>
2016-06-03 11:37:57 +0800

    删除调整package后的文件

--- sncp-cbss/cbss.core/cbss.model/trunk/src/main/java/cbss/core/model/request/Param.java
+++
@@ -1,9 +0,0 @@
-package cbss.core.model.request;
-
-import java.io.Serializable;
-
-public interface Param extends Serializable {
-
-	public boolean checkParamValueIn()
-			throws ParamValueValidateException;
-}
