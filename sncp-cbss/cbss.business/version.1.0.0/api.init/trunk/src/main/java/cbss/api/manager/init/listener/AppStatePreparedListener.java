ae9d1ca96e4a3056469bac5b42a20bb34dec7105
maocc@snail.com <maocc@snail.com>
2016-06-07 10:08:22 +0800

    添加心跳发送

---
+++ sncp-cbss/cbss.business/version.1.0.0/api.init/trunk/src/main/java/cbss/api/manager/init/listener/AppStatePreparedListener.java
@@ -0,0 +1,15 @@
+package cbss.api.manager.init.listener;
+
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.context.ApplicationListener;
+
+import cbss.core.trace.aspect.listener.Trace;
+
+public class AppStatePreparedListener implements ApplicationListener<ApplicationPreparedEvent> {
+
+	@Override
+	public void onApplicationEvent(ApplicationPreparedEvent event) {
+		Trace.applicationState(event, event.getTimestamp(), event.getArgs(), event.getSpringApplication());
+	}
+
+}
