191c3a44d613051ab3d0d6a28291f824568376a5
iekr <iekr@qq.com>
2016-06-28 17:34:46 +0800

    新增手机下载管理

---
+++ sncp-cbss/cbss.business/version.1.0.0/api.appMembers/trunk/src/main/java/com/woniu/sncp/cbss/api/conf/controller/AppConfRequestParam.java
@@ -0,0 +1,90 @@
+package com.woniu.sncp.cbss.api.conf.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.woniu.sncp.cbss.core.model.request.ParamValueValidateException;
+import com.woniu.sncp.cbss.core.model.request.RequestParam;
+
+public class AppConfRequestParam extends RequestParam {
+
+	private static final long serialVersionUID = 8672987272378909273L;
+	private String type;
+	private String osType;
+	private int pageSize;
+	private int pageNumber;
+
+	private Long totalSize;//总条数
+	
+	private List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+	
+	public List<Map<String, Object>> getList() {
+		return list;
+	}
+
+	public void setList(List<Map<String, Object>> list) {
+		this.list = list;
+	}
+
+	public Long getTotalSize() {
+		return totalSize;
+	}
+
+	public void setTotalSize(Long totalSize) {
+		this.totalSize = totalSize;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getOsType() {
+		return osType;
+	}
+
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	@Override
+	public boolean checkParamValueIn()
+			throws ParamValueValidateException {
+		String type = getType();
+		if(StringUtils.isBlank(type)){
+			return false;
+		}
+		String osType = getOsType();
+		if(StringUtils.isBlank(osType)){
+			return false;
+		}
+		int page = getPageSize();
+		int number = getPageNumber();
+		if(number > 0 && page <= 0){
+			return false;
+		}
+		return true;
+	}
+}
---
+++ sncp-cbss/cbss.business/version.1.0.0/api.appMembers/trunk/src/main/java/com/woniu/sncp/cbss/api/conf/controller/ConfController.java
@@ -0,0 +1,90 @@
+package com.woniu.sncp.cbss.api.conf.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.woniu.sncp.cbss.core.authorize.AccessAuthorizeFilterConfigures;
+import com.woniu.sncp.cbss.core.errorcode.EchoInfo;
+import com.woniu.sncp.cbss.core.errorcode.ErrorCode;
+import com.woniu.sncp.exception.MissingParamsException;
+import com.woniu.sncp.profile.dto.DownConfigTo;
+import com.woniu.sncp.profile.dto.PaginationTo;
+import com.woniu.sncp.profile.service.DownConfigService;
+
+@RestController
+@RequestMapping(AccessAuthorizeFilterConfigures.BASE_CONTEXT)
+@Configuration
+public class ConfController {
+
+	private static final Logger logger = LoggerFactory.getLogger(ErrorCode.class);
+	@Autowired
+	private DownConfigService downConfigService;
+	@Autowired
+	private ErrorCode errorCode;
+
+	/**
+	 * eai中修改防沉迷开关后,调此接口把开关数据同步过去
+	 * 
+	 * @param requestDatas
+	 * @return
+	 */
+	@RequestMapping(value = "/app/members/conf", method = RequestMethod.POST)
+	@ResponseBody
+	public EchoInfo<Object> appMembersConf(@RequestBody AppConfRequestDatas requestDatas) {
+		AppConfRequestParam data = requestDatas.getParamdata();
+		String type = data.getType();
+		String osType = data.getOsType();
+		int pageSize = data.getPageSize();
+		int number = data.getPageNumber();
+
+		if (StringUtils.isBlank(type) || StringUtils.isBlank(osType)) {
+			return errorCode.getErrorCode(1001, requestDatas.getSessionId());
+		}
+
+		try {
+			PaginationTo page = downConfigService.query(type, osType, pageSize, number);
+			if(page == null){
+				return errorCode.getErrorCode(10007, requestDatas.getSessionId());
+			}
+			EchoInfo<Object> result = errorCode.getErrorCode(1, requestDatas.getSessionId());
+			List<DownConfigTo> downConfigList = page.getDownConfigList();
+			List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+			for (DownConfigTo to : downConfigList) {
+				Map<String,Object> map = new HashMap<String,Object>();
+				map.put("type", to.getType());
+				map.put("name", to.getName());
+				map.put("icoUrl", to.getIco());
+				map.put("downUrl", to.getDownUrl());
+				map.put("desc", to.getDesc());
+				map.put("osType", to.getOsType());
+				list.add(map);
+			}
+			AppConfRequestParam d = new AppConfRequestParam();
+			d.setPageNumber(page.getPageNumber());
+			d.setPageSize(page.getPageSize());
+			d.setTotalSize(page.getTotalSize());
+			d.setList(list);
+			result.setData(d);
+			return result;
+		} catch (MissingParamsException e) {
+			logger.error("appMembersConf", e);
+			return errorCode.getErrorCode(10001, requestDatas.getSessionId());
+		} catch (Exception e) {
+			logger.error("appMembersConf", e);
+			return errorCode.getErrorCode(10002, requestDatas.getSessionId());
+		}
+	}
+}
