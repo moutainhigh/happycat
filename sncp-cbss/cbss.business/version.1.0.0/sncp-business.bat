97ded3ebd555f33778e3174fda9dcd1984ee1e82
maocc@snail.com <maocc@snail.com>
2016-06-29 18:45:50 +0800

    修改脚本

--- sncp-cbss/cbss.business/version.1.0.0/sncp-business.bat
+++ sncp-cbss/cbss.business/version.1.0.0/sncp-business.bat
@@ -2,13 +2,13 @@
 cd %CD%
 
 call cd %CD%\api.parent
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -Dmaven.test.skip=true
 
 call cd ..\api.init\trunk
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -Dmaven.test.skip=true
 
 call cd ..\..\api.fcm\trunk
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -Dmaven.test.skip=true
 
 call cd ..\..\api.appMembers\trunk
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -Dmaven.test.skip=true
