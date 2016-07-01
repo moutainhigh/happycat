9cdc13a5826c7578d81a44030e41f4e784cc7e22
luzz <luzz@snail.com>
2016-07-01 16:19:23 +0800

    删除其他main方法

--- sncp-service/sncp-profile/profile-dubbo/src/main/java/com/woniu/sncp/profile/config/EncryptUtils.java
+++ sncp-service/sncp-profile/profile-dubbo/src/main/java/com/woniu/sncp/profile/config/EncryptUtils.java
@@ -68,53 +68,53 @@ public class EncryptUtils {
         }
     }
     
-    public static void main(String[] args) throws Exception {//提供了Java命令使用该工具的功能
-        if (args.length == 2 && args[0].equals("key")) {// 生成密钥文件
-            Key key = EncryptUtils.createKey();
-            String path = System.getProperty("user.dir") + "/" + args[1];
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
-            oos.writeObject(key);
-            oos.close();
-            System.out.println("gen encrypt key: " + path);
-        } else if (args.length == 3 && args[0].equals("encrypt")) {//对文件进行加密
+//    public static void main(String[] args) throws Exception {//提供了Java命令使用该工具的功能
+//        if (args.length == 2 && args[0].equals("key")) {// 生成密钥文件
+//            Key key = EncryptUtils.createKey();
+//            String path = System.getProperty("user.dir") + "/" + args[1];
+//            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+//            oos.writeObject(key);
+//            oos.close();
+//            System.out.println("gen encrypt key: " + path);
+//        } else if (args.length == 3 && args[0].equals("encrypt")) {//对文件进行加密
+////            File file = new File(System.getProperty("user.dir") + "/" + args[1]);
+//            File file = new File("/opt/security/db/" + args[1]);
+//            FileInputStream in = new FileInputStream(file);
+//            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+//            byte[] tmpbuf = new byte[1024];
+//            int count = 0;
+//            while ((count = in.read(tmpbuf)) != -1) {
+//                bout.write(tmpbuf, 0, count);
+//                tmpbuf = new byte[1024];
+//            }
+//            in.close();
+//            byte[] orgData = bout.toByteArray();
+//            Key key = getKey(new FileInputStream("/opt/security/db/" + args[2]));
+//            byte[] raw = EncryptUtils.doEncrypt(key, orgData);
+//            file = new File(file.getParent() + "\\en_" + file.getName());
+//            FileOutputStream out = new FileOutputStream(file);
+//            out.write(raw);
+//            out.close();
+//            System.out.println("成功加密，加密文件位于:"+file.getAbsolutePath());
+//        } else if (args.length == 3 && args[0].equals("decrypt")) {//对文件进行解密
 //            File file = new File(System.getProperty("user.dir") + "/" + args[1]);
-            File file = new File("/opt/security/db/" + args[1]);
-            FileInputStream in = new FileInputStream(file);
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            byte[] tmpbuf = new byte[1024];
-            int count = 0;
-            while ((count = in.read(tmpbuf)) != -1) {
-                bout.write(tmpbuf, 0, count);
-                tmpbuf = new byte[1024];
-            }
-            in.close();
-            byte[] orgData = bout.toByteArray();
-            Key key = getKey(new FileInputStream("/opt/security/db/" + args[2]));
-            byte[] raw = EncryptUtils.doEncrypt(key, orgData);
-            file = new File(file.getParent() + "\\en_" + file.getName());
-            FileOutputStream out = new FileOutputStream(file);
-            out.write(raw);
-            out.close();
-            System.out.println("成功加密，加密文件位于:"+file.getAbsolutePath());
-        } else if (args.length == 3 && args[0].equals("decrypt")) {//对文件进行解密
-            File file = new File(System.getProperty("user.dir") + "/" + args[1]);
-            FileInputStream fis = new FileInputStream(file);
-            Key key = getKey(new FileInputStream(args[2]));
-            InputStream raw = EncryptUtils.doDecrypt(key, fis);
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            byte[] tmpbuf = new byte[1024];
-            int count = 0;
-            while ((count = raw.read(tmpbuf)) != -1) {
-                bout.write(tmpbuf, 0, count);
-                tmpbuf = new byte[1024];
-            }
-            raw.close();
-            byte[] orgData = bout.toByteArray();
-            file = new File(file.getParent() + "\\rs_" + file.getName());
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(orgData);
-            System.out.println("成功解密，解密文件位于:"+file.getAbsolutePath());
-        }
-    }
+//            FileInputStream fis = new FileInputStream(file);
+//            Key key = getKey(new FileInputStream(args[2]));
+//            InputStream raw = EncryptUtils.doDecrypt(key, fis);
+//            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+//            byte[] tmpbuf = new byte[1024];
+//            int count = 0;
+//            while ((count = raw.read(tmpbuf)) != -1) {
+//                bout.write(tmpbuf, 0, count);
+//                tmpbuf = new byte[1024];
+//            }
+//            raw.close();
+//            byte[] orgData = bout.toByteArray();
+//            file = new File(file.getParent() + "\\rs_" + file.getName());
+//            FileOutputStream fos = new FileOutputStream(file);
+//            fos.write(orgData);
+//            System.out.println("成功解密，解密文件位于:"+file.getAbsolutePath());
+//        }
+//    }
     
 }
