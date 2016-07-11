127345a77807a2f8d0d297aaa0dac69bf30bb016
maocc@snail.com <maocc@snail.com>
2016-07-11 11:35:34 +0800

    调整测试脚本

--- sncp-cbss/cbss.business/version.1.0.0/api.fcm/trunk/src/test/java/Test.java
+++ sncp-cbss/cbss.business/version.1.0.0/api.fcm/trunk/src/test/java/Test.java
@@ -21,19 +21,19 @@ public class Test {
 
 	public static void main(final String[] args) {
 		try {
-			// TSocket transport = new TSocket("10.103.4.137", 13001);
-			TSocket transport = new TSocket("172.18.70.180", 8080);
+			 TSocket transport = new TSocket("10.103.4.137", 12001);
+//			TSocket transport = new TSocket("172.18.70.180", 8080);
 			transport.open();
 			TBinaryProtocol protocol = new TBinaryProtocol(transport);
 			TMultiplexedProtocol protocol1 = new TMultiplexedProtocol(protocol, "api.fcmapiservice");
 			Api.Client client = new Api.Client(protocol1);
 			long time = System.currentTimeMillis();
-			for (int i = 0; i < 1000; i++) {
+			for (int i = 0; i < 1; i++) {
 				Access access = new Access(203, 9, "l47Bz5vboMtfwa5", new HashMap<String, String>(0));
 				FcmConfRequestParam nciicRequestParam = new FcmConfRequestParam();
 				nciicRequestParam.setGameIds("1,2,3,4");
 				nciicRequestParam.setIssuerId(7L);
-				Data data = new Data("1.0.0", new ClientRequest(System.currentTimeMillis(), "172.18.70.180", "172.18.70.180", new HashMap<String, String>(0)), new Param(
+				Data data = new Data(ApiConstants.VERSION, new ClientRequest(System.currentTimeMillis(), "172.18.70.180", "172.18.70.180", new HashMap<String, String>(0)), new Param(
 						FcmConfRequestParam.class.getName(), ApiConstants.PARAM_RESOLVE_TYPE_DEFAULT, JSONObject.toJSONString(nciicRequestParam), new ArrayList<String>(0)),
 						ApiConstants.TRACE_OPEN_CLIENT_STATE, "1");
 				Signature signature = new Signature(1, "83FD67BBB5535E30E558B605747A8E7D");
