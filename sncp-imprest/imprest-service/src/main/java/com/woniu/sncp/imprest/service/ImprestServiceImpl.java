99cf698d9b5ed92a63995e6b0d183670a51ed7bc
luzz <luzz@snail.com>
2016-08-08 16:34:49 +0800

    查询充值记录列表

--- sncp-imprest/imprest-service/src/main/java/com/woniu/sncp/imprest/service/ImprestServiceImpl.java
+++ sncp-imprest/imprest-service/src/main/java/com/woniu/sncp/imprest/service/ImprestServiceImpl.java
@@ -1,5 +1,8 @@
 package com.woniu.sncp.imprest.service;
 
+import java.util.Date;
+import java.util.List;
+
 import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +15,15 @@ import com.woniu.sncp.imprest.entity.ImprestCardType;
 import com.woniu.sncp.imprest.entity.ImprestLog;
 import com.woniu.sncp.imprest.entity.ImprestOrder;
 import com.woniu.sncp.imprest.repository.ImprestCardTypeRepository;
+import com.woniu.sncp.imprest.repository.ImprestLogDao;
 import com.woniu.sncp.imprest.repository.ImprestLogRepository;
 import com.woniu.sncp.imprest.repository.ImprestOrderRepository;
 
 @Service
 public class ImprestServiceImpl implements ImprestService {
+	
+	@Autowired
+	protected ImprestLogDao imprestLogDao;
 	
 	@Autowired
 	private ImprestLogRepository imprestLogRepository;
@@ -47,6 +54,15 @@ public class ImprestServiceImpl implements ImprestService {
 		DozerBeanMapper beanMapper = new DozerBeanMapper();
 		ImprestCardType imprestCardType = imprestCardTypeRepository.findOne(cardId);
 		return beanMapper.map(imprestCardType, ImprestCardTypeDTO.class);
+	}
+
+	@Override
+	public List<ImprestLogDTO> queryImprestLogs(Long aid, Long gameId, Long areaId, List<Long> platformIds,
+			Date startDate, Date endDate, List<Long> speCards) throws SystemException {
+		
+		List<ImprestLogDTO> impLogList = imprestLogDao.queryImprestLogs(aid, gameId, areaId, platformIds, startDate, endDate, speCards);
+		
+		return impLogList;
 	}
 
 }
