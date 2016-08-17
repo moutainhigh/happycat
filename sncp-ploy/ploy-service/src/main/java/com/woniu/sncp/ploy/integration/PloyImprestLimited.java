df1429b15630094b120aec5ff620041e089a1bcc
chenyx <chenyx@snail.com>
2016-08-17 19:00:01 +0800

    活动限制更新

--- sncp-ploy/ploy-service/src/main/java/com/woniu/sncp/ploy/integration/PloyImprestLimited.java
+++ sncp-ploy/ploy-service/src/main/java/com/woniu/sncp/ploy/integration/PloyImprestLimited.java
@@ -1,5 +1,6 @@
 package com.woniu.sncp.ploy.integration;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -16,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.util.Assert;
 
-import com.woniu.sncp.core.errorcode.ErrorCode;
 import com.woniu.sncp.imprest.dto.ImprestCardTypeDTO;
 import com.woniu.sncp.imprest.dto.ImprestLogDTO;
 import com.woniu.sncp.imprest.service.ImprestService;
@@ -25,30 +25,31 @@ import com.woniu.sncp.ploy.domain.PloyParticipator;
 import com.woniu.sncp.ploy.dto.PloyRequestDTO;
 import com.woniu.sncp.ploy.dto.PloyTypeStatDTO;
 
-
 /**
  * 活动充值限制
+ * 
  * @author chenyx
  *
  */
 public class PloyImprestLimited {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(PloyImprestLimited.class);
-	
+
 	@Autowired
 	private ImprestService imprestService;
 
-	
 	/**
 	 * 充值指定卡类型首充限制
+	 * 
 	 * @param input
 	 * @return
 	 */
-	public Message<PloyTypeStatDTO>  speCardsFirstLimited(Message<PloyTypeStatDTO> input) {
-		String _speCardsFirst = StringUtils.substringBetween(input.getPayload().getPresentsPloy().getLimitContent() + "|", "specardsfirst:", "|");
-		if(StringUtils.isNotBlank(_speCardsFirst)) {
-			ImprestCardTypeDTO imprestCardTypeDTO = input.getHeaders().get("ImprestCardType",ImprestCardTypeDTO.class);
-			Assert.notNull("ImprestCardType","ImprestCardType is null ");
+	public Message<PloyTypeStatDTO> speCardsFirstLimited(Message<PloyTypeStatDTO> input) {
+		String _speCardsFirst = StringUtils
+				.substringBetween(input.getPayload().getPresentsPloy().getLimitContent() + "|", "specardsfirst:", "|");
+		if (StringUtils.isNotBlank(_speCardsFirst)) {
+			ImprestCardTypeDTO imprestCardTypeDTO = input.getHeaders().get("ImprestCardType", ImprestCardTypeDTO.class);
+			Assert.notNull("ImprestCardType", "ImprestCardType is null ");
 			PassportDto passportDto = input.getHeaders().get("Passport", PassportDto.class);
 			Assert.isNull(passportDto, "passport is null");
 			String[] _speCards = null;
@@ -61,9 +62,10 @@ public class PloyImprestLimited {
 						if (speCard != 0)
 							speCards.add(speCard);
 					}
-					if (CollectionUtils.isNotEmpty(speCards) && !speCards.contains(imprestCardTypeDTO.getId().toString())) {
-						logger.error(passportDto.getId() + ",指定充值返点活动:" + input.getPayload().getPresentsPloy().getId() + 
-								",不在指定的充值卡内:" + _speCardsFirst + ",本次充值卡:" + imprestCardTypeDTO.getId());
+					if (CollectionUtils.isNotEmpty(speCards)
+							&& !speCards.contains(imprestCardTypeDTO.getId().toString())) {
+						logger.error(passportDto.getId() + ",指定充值返点活动:" + input.getPayload().getPresentsPloy().getId()
+								+ ",不在指定的充值卡内:" + _speCardsFirst + ",本次充值卡:" + imprestCardTypeDTO.getId());
 						input.getPayload().clean();
 					}
 				}
@@ -71,16 +73,16 @@ public class PloyImprestLimited {
 		}
 		return input;
 	}
-	
-	
+
 	/**
 	 * 最多返点值限制
+	 * 
 	 * @param input
 	 * @return
 	 */
-	public Message<PloyTypeStatDTO>  returnMaxPointLimited(Message<PloyTypeStatDTO> input) {
-		String returnMaxPoint = StringUtils.substringBetween(input.getPayload().getPresentsPloy().getLimitContent() + "|",
-				 "ReturnMaxPoint:", "|");
+	public Message<PloyTypeStatDTO> returnMaxPointLimited(Message<PloyTypeStatDTO> input) {
+		String returnMaxPoint = StringUtils
+				.substringBetween(input.getPayload().getPresentsPloy().getLimitContent() + "|", "ReturnMaxPoint:", "|");
 		logger.debug("充值累计返点活动，本次返点最大值为 :" + returnMaxPoint);
 		if (StringUtils.isNotBlank(returnMaxPoint)) {
 			PassportDto passportDto = input.getHeaders().get("Passport", PassportDto.class);
@@ -88,26 +90,29 @@ public class PloyImprestLimited {
 			ImprestLogDTO imprestLog = input.getHeaders().get("ImprestLog", ImprestLogDTO.class);
 			Assert.isNull(imprestLog, "imprestLog is null");
 			Date start = DateUtils.truncate(imprestLog.getImprestDate(), Calendar.DATE);
-			Date end = DateUtils.truncate( DateUtils.addDays(imprestLog.getImprestDate(), 1), Calendar.DATE);
-			//查询用户返点记录
-			Long returnAmount = imprestService.findSumLargessPoints(passportDto.getId(), start, end, input.getPayload().getPresentsPloy().getType(), null);
-			if(returnAmount == null || returnAmount.longValue() >= Long.valueOf(returnMaxPoint).longValue()) {
+			Date end = DateUtils.truncate(DateUtils.addDays(imprestLog.getImprestDate(), 1), Calendar.DATE);
+			// 查询用户返点记录
+			BigDecimal returnAmount = imprestService.findSumLargessPoints(passportDto.getId(), start, end,
+					input.getPayload().getPresentsPloy().getType(), null);
+			if (returnAmount == null || returnAmount.longValue() >= Long.valueOf(returnMaxPoint).longValue()) {
 				logger.debug("充值累计返点活动 最高返回，不再赠送.....");
-				logger.debug("passportId: " + passportDto.getId() + ",已返点记录: " + returnAmount + ",本次返点最大值: " + returnMaxPoint);
+				logger.debug("passportId: " + passportDto.getId() + ",已返点记录: " + returnAmount + ",本次返点最大值: "
+						+ returnMaxPoint);
 				input.getPayload().clean();
 			}
 		}
 		return input;
 	}
-	
+
 	/**
 	 * 充值总额限制条件，必须大于充值总额
+	 * 
 	 * @param input
 	 * @return
 	 */
 	public Message<PloyTypeStatDTO> imprestAmountLimited(Message<PloyTypeStatDTO> input) {
-		String imprestAmount = StringUtils.substringBetween(input.getPayload().getPresentsPloy().getLimitContent() + "|",
-				 "imprestAmount:", "|");	
+		String imprestAmount = StringUtils
+				.substringBetween(input.getPayload().getPresentsPloy().getLimitContent() + "|", "imprestAmount:", "|");
 		if (StringUtils.isNotBlank(imprestAmount)) {
 			PloyParticipator participator = (PloyParticipator) input.getHeaders().get("ployParticipatorFactory");
 			PloyRequestDTO ployRequestDTO = participator.getPloyRequest();
@@ -116,12 +121,32 @@ public class PloyImprestLimited {
 			ImprestLogDTO imprestLog = input.getHeaders().get("ImprestLog", ImprestLogDTO.class);
 			Assert.isNull(imprestLog, "imprestLog is null");
 			Date start = DateUtils.truncate(imprestLog.getImprestDate(), Calendar.DATE);
-			Date end = DateUtils.truncate( DateUtils.addDays(imprestLog.getImprestDate(), 1), Calendar.DATE);
-            Long _imprestAmount = imprestService.findSumImprestAmount(Long.valueOf(ployRequestDTO.getGameId()), passportDto.getId(), start, end);
-            if (_imprestAmount == null || _imprestAmount.longValue() < Long.valueOf(imprestAmount).longValue()) {
-            	input.getPayload().clean();
-            }
-        }
+			Date end = DateUtils.truncate(DateUtils.addDays(imprestLog.getImprestDate(), 1), Calendar.DATE);
+			BigDecimal _imprestAmount = imprestService.findSumImprestAmount(Long.valueOf(ployRequestDTO.getGameId()),
+					passportDto.getId(), start, end);
+			if (_imprestAmount == null || _imprestAmount.longValue() < Long.valueOf(imprestAmount).longValue()) {
+				input.getPayload().clean();
+			}
+		}
 		return input;
 	}
+
+	public Message<PloyTypeStatDTO> imprestMinAmountLimited(Message<PloyTypeStatDTO> input) {
+		String _impmoney = StringUtils.substringBetween(input.getPayload().getPresentsPloy().getLimitContent() + "|",
+				"min:", "|");
+		if (StringUtils.isNotBlank(_impmoney)) {
+			PloyParticipator participator = (PloyParticipator) input.getHeaders().get("ployParticipatorFactory");
+			PloyRequestDTO ployRequestDTO = participator.getPloyRequest();
+			PassportDto passportDto = input.getHeaders().get("Passport", PassportDto.class);
+			Assert.isNull(passportDto, "passport is null");
+			BigDecimal impMoney = imprestService.findSumImprestAmount(passportDto.getId(), ployRequestDTO.getGameId(),
+					ployRequestDTO.getGameAreaId(), "A", input.getPayload().getPresentsPloy().getStart(), input.getPayload().getPresentsPloy().getEnd());
+			logger.debug("充值配送金额未设置或者设置不正确," + input.getPayload().getPresentsPloy().getType());
+			 
+			return null;
+		}
+
+		logger.debug("充值配送活动累计个人充值 : " + impMoney + " 活动配置 最低金额: " + _impmoney);
+	}
+
 }
