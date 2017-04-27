3f3d6a6a50a41ed1b4bdaf1864615f8b460c9328
fuzl <fuzl@mysnail.com>
2017-04-27 18:49:53 +0800

    收银台：新增健康检查

--- sncp-cashier/cashier-api/src/main/java/com/woniu/sncp/pay/web/api/HealthController.java
+++ sncp-cashier/cashier-api/src/main/java/com/woniu/sncp/pay/web/api/HealthController.java
@@ -7,6 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Health.Builder;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +23,7 @@ import com.woniu.sncp.pay.core.service.CoreDbService;
  * @Copyright 2017 Snail Soft, Inc. All rights reserved.
  */
 @RestController
-public class HealthController {
+public class HealthController implements HealthIndicator{
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -77,5 +80,37 @@ public class HealthController {
 			response.setStatus(500);
 		}
 		return false;
+	}
+
+	@Override
+	public Health health() {
+		Builder down = Health.down();
+		try {
+			// 1. 确认是否需要检查
+        	if(CoreDbService.DB_OPEN_CHECK.equals(openCheck)){
+        		// 2. 查询当前db 主机状态
+        		String dbState = coreDbService.getDbState(machineName);
+        		if(StringUtils.isNotBlank(dbState)){
+        			if(!CoreDbService.DB_MACHINE_STATE_ONLINE.equals(dbState)){
+    					//状态不在线,终止操作
+    					logger.info("当前机器状态:{},{}",dbState,"请求终止");
+    					return down.build();
+    				}
+        		}else{
+        			//获取不到db机器状态,终止操作
+        			return down.build();
+        		}
+        	}
+        	
+        	// 3.查询测试sql
+			String queryResult = coreDbService.getQueryTestResult();
+			if(queryResult.endsWith("1")){
+				return Health.up().build();
+			}
+		} catch (Exception e) {
+			logger.error(this.getClass().getSimpleName(), e);
+			return down.build();
+		}
+		return Health.up().build();
 	}
 }
