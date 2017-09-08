17f3c883bc58b28925212c157d7807c5f2b20c3b
wenyd <wenyadong@snail.com>
2017-09-08 17:10:50 +0800

    手续费

--- sncp-cashier/cashier-domain/src/main/java/com/woniu/sncp/pay/repository/pay/PaymentOrderDiscountRecordRepository.java
+++ sncp-cashier/cashier-domain/src/main/java/com/woniu/sncp/pay/repository/pay/PaymentOrderDiscountRecordRepository.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.woniu.sncp.pojo.payment.PaymentOrderDiscount;
 import com.woniu.sncp.pojo.payment.PaymentOrderDiscountRecord;
@@ -22,11 +23,6 @@ import com.woniu.sncp.pojo.payment.PaymentOrderDiscountRecord;
  */
 @Transactional
 public interface PaymentOrderDiscountRecordRepository extends JpaRepository<PaymentOrderDiscountRecord, Long> {
-//"from  PaymentOrderDiscount where merchantId=? and paymentId=? and state=? and validityPeriodStart<=? and validityPeriodEnd>=?",new Object[] {Long.valueOf(merchantId),Long.valueOf(paymentId),"1",time,time}
-	@Query("from  PaymentOrderDiscount where merchantId=:merchantId  and paymentId=:paymentId and state='1' and validityPeriodStart<=:now and validityPeriodEnd>=:now")
-	List<PaymentOrderDiscount> queryOrderDiscount(long merchantId, long paymentId,Date now) ;
-	@Query("from  PaymentOrderDiscount where merchantId=:merchantId  and state='1' and validityPeriodStart<=:now and validityPeriodEnd>=:now")
-	List<PaymentOrderDiscount> queryOrderDiscount(long merchantId, Date now) ;
-	
+ 
 	
 }
